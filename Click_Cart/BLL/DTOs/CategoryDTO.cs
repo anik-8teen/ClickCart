6adfac254e0b75e88e0def8af2797c05ed384431
anik_8teen <20-42695-1@student.aiub.edu>
2023-05-01 03:50:14 +0600

    DTOs ADDED

---
+++ Click_Cart/BLL/DTOs/CategoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class CategoryDTO
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
